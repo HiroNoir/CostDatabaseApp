8b5a331284cd7473f111d5c26df17e1fa9454ff1
Hiroaki.Yamamoto <noir.noir@hotmail.co.jp>
2024-07-06 22:40:56 +0900

    工事契約の登録・更新画面を実装（処理は▲未実装）

--- src/main/java/com/example/demo/controller/ConstructionContractController.java
+++ src/main/java/com/example/demo/controller/ConstructionContractController.java
@@ -3,11 +3,14 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.entity.ConstructionContract;
+import com.example.demo.form.ConstructionContractForm;
+import com.example.demo.helper.ConstructionContractHelper;
 import com.example.demo.service.ConstructionContractService;
 
 import lombok.RequiredArgsConstructor;
@@ -62,5 +65,69 @@ public class ConstructionContractController {
         }
 
     }
+
+    /** 【登録画面表示】　*/
+    @GetMapping("/create")
+    public String create(@ModelAttribute ConstructionContractForm form) {
+
+        // @ModelAttributeの引数省略型を利用しているため、下記のように、Model名はクラス名のローワーキャメルケースとなる
+        // model.addAttribute("constructionContractForm", form);　→form.htmlへ引き継ぐModel名となる
+        // 更新画面表示・更新処理実行のメソッドにおいても上記と同様のModel名とする
+
+        /** 登録画面へ遷移 */
+        // 登録画面としてform.htmlが実行されるよう設定
+        form.setIsNew(true);
+        // 登録画面へ遷移（アドレス指定）
+        return "construction-contract/form";
+
+    }
+
+    /** 【登録処理実行】 */
+    // ▲未実装
+
+    /** 【更新画面表示】 */
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") Integer ccId,
+            Model model, RedirectAttributes redirectAttributes) {
+
+        /** 更新画面へ遷移　その1　*/
+        // idがnullの場合は更新処理実行時の入力チェックでひっかかったため再度更新画面へ遷移する
+        if(ccId == null) {
+            // 更新画面へ遷移（アドレス指定）
+            return "construction-contract/form";
+        }
+
+        /** 更新画面へ遷移　その2 */
+        // 更新画面へ遷移　その1で、idがnullでない場合は新規で更新画面へ遷移する
+        // 更新画面への遷移はGETメソッドでid入力可能のため、URLでidを直入力された場合の、対象データの有無チェックを行う
+        // 対象データを取得
+        ConstructionContract target = service.findById(ccId);
+        // 対象データの有無確認
+        if (target != null) {
+            // 対象データがある場合は処理を進める
+            // EntityからFormへ変換
+            ConstructionContractForm form = ConstructionContractHelper.convertForm(target);
+            // Modelに格納
+            //　登録画面表示の@ModelAttribute引数省略型に合せ、Model名はFormクラス名のローワーキャメルケースとする
+            model.addAttribute("constructionContractForm", form);
+            // 更新画面としてform.htmlが実行されるよう設定
+            form.setIsNew(false);
+            // 更新画面へ遷移（アドレス指定）
+            return "construction-contract/form";
+        } else {
+            // 対象データがない場合は一覧画面へ戻る
+            // エラーのフラッシュメッセージをRedirectAttributesに格納
+            redirectAttributes.addFlashAttribute("errorMessage", "対象データがありません");
+            // 一覧画面へリダイレクト（アドレス指定）
+            return "redirect:/construction-contract/list";
+        }
+
+    }
+
+    /**　【更新処理実行】 */
+    // ▲未実装
+
+    /** 【削除処理実行】 */
+    // ▲未実装
 
 }
---
+++ src/main/java/com/example/demo/form/ConstructionContractForm.java
@@ -0,0 +1,82 @@
+package com.example.demo.form;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+* 工事契約フォームクラス
+*/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConstructionContractForm {
+
+    /** ID */
+    private Integer ccId;
+
+    /** 内訳種別 */
+    private Integer ccEtId;
+
+    /** 積算時期（年） */
+    @NotBlank(message = "積算時期（年）は必須です。")
+    @Size(max = 10, message = "{max}文字以下で入力してください。")
+    private String estimateYear;
+
+    /** 積算時期（月） */
+    @NotBlank(message = "積算時期（月）は必須です。")
+    @Size(max = 10, message = "{max}文字以下で入力してください。")
+    private String estimateMonth;
+
+    /** 発注工期 */
+    @NotBlank(message = "発注工期は必須です。")
+    @Size(max = 10, message = "{max}文字以下で入力してください。")
+    private String constructionPeriod;
+
+    /** 工事名称 */
+    @NotBlank(message = "工事名称は必須です。")
+    @Size(max = 100, message = "{max}文字以下で入力してください。")
+    private String projectName;
+
+    /** 工事場所 */
+    @NotBlank(message = "工事場所は必須です。")
+    @Size(max = 100, message = "{max}文字以下で入力してください。")
+    private String siteAddress;
+
+    /** 敷地面積 */
+    @NotBlank(message = "敷地面積は必須です。")
+    private double siteArea;
+
+    /** 別途工事 */
+    @NotBlank(message = "別途工事は必須です。")
+    @Size(max = 100, message = "{max}文字以下で入力してください。")
+    private String separeteConstruction;
+
+    /** 予定価格 */
+    @NotBlank(message = "予定価格は必須です。")
+    private long plannedPrice;
+
+    /** 落札価格 */
+    @NotBlank(message = "落札価格は必須です。")
+    private long contractPrice;
+
+    /** 施工業者 */
+    @NotBlank(message = "施工業者は必須です。")
+    @Size(max = 30, message = "{max}文字以下で入力してください。")
+    private String contractorName;
+
+    /** 経緯等コメント */
+    @NotBlank(message = "経緯等コメントは必須です。特になければ「特に無し」などと記録に残してください。")
+    private String remarksSection;
+
+    /** 発注図（抜粋）保存先 */
+    @NotBlank(message = "発注図（抜粋）保存先は必須です。未定の場合は「決まり次第更新」など記録を残してください。")
+    private String blueprintAddress;
+
+    /** 新規判定 */
+    // 従業員新規登録の場合はTrue、更新の場合はfalse
+    private Boolean isNew;
+
+}
