e25bea90afa0c8287564f726749f85ec8bb771c0
Hiroaki.Yamamoto <noir.noir@hotmail.co.jp>
2024-08-13 23:27:59 +0900

    BreakdownCsControllerの特定取得メソッド記述見直し修正

--- src/main/java/com/example/demo/controller/BreakdownCsController.java
+++ src/main/java/com/example/demo/controller/BreakdownCsController.java
@@ -69,69 +69,37 @@ public class BreakdownCsController {
     public String specify(@PathVariable("id") Integer bcsBcdId,
             Model model, RedirectAttributes redirectAttributes) {
 
-        /** 現在表示している工事契約、内訳頭紙区分、内訳頭紙金額を取得 */
-        // GETメソッドでid入力可能のため、URLでidを直入力された場合の、対象データの有無チェックを行う
-        // 対象データが存在しない場合IndexOutOfBoundsExceptionを吐くのでtry-catchで対応
-        try {
-            // 対象データがある場合は処理を進める
-            // 対象データを取得
-            BreakdownCd breakdownCd = breakdownCdService.findByBcdId(bcsBcdId);
-            BreakdownCo breakdownCo = breakdownCoService.findByBcoId(breakdownCd.getBcdBcoId());
-            // 工事契約を取得
-            Integer ccId = breakdownCo.getBcoCcId();
-            ConstructionContract targetConstructionContract = constructionContractService.findById(ccId);
-            // Modelに格納
-            model.addAttribute("projectName", targetConstructionContract.getProjectName());
-            model.addAttribute("ccId", targetConstructionContract.getCcId());
-            // 内訳頭紙区分を取得
-            Integer coId = breakdownCo.getBcoCoId();
-            CategoryOutline targetCategoryOutline = categoryOutlineService.findById(coId);
-            // Modelに格納
-            model.addAttribute("coTypeName", targetCategoryOutline.getCoTypeName());
-            model.addAttribute("coId", targetCategoryOutline.getCoId());
-            // 内訳種目区分を取得
-            Integer cdId = breakdownCd.getBcdCdId();
-            CategoryDetail targetCategoryDetail = categoryDetailService.findById(cdId);
-            // Modelに格納
-            model.addAttribute("cdTypeName", targetCategoryDetail.getCdTypeName());
-            model.addAttribute("cdId", targetCategoryDetail.getCdId());
-            // 内訳種目を取得
-            // Modelに格納
-            model.addAttribute("bcdTypeName", breakdownCd.getBcdTypeName());
-            model.addAttribute("bcdId", breakdownCd.getBcdId());
-            model.addAttribute("bcdBcoId", breakdownCd.getBcdBcoId());
-        } catch (NullPointerException e) {
-            // 対象データがない場合は一覧画面へ戻る
-            //　エラーのフラッシュメッセージをRedirectAttributesに格納し一覧画面へ戻る
-            redirectAttributes.addFlashAttribute("errorMessage", "対象データがありません");
-            // 一覧画面へリダイレクト（アドレス指定）
-            return "redirect:/construction-contract/list";
-        }
-
         /** ローカルフィールド定義、及び、初期化 */
         Long longDirectConstructionPrice = null;     // breakdown_cdテーブルより取得した各種目の直接工事費
         Long longSumDirectConstructionPrice = null;  // breakdown_cdテーブルより取得した各種目の直接工事費
 
         /** 現在表示している内訳種目の各金額をbreakdown_cdテーブルより取得 */
         // 対象データを取得
-        // 金額が入力されていない場合NullPointerExceptionを吐くのでtry-catchで対応
-        try {
-            BreakdownCd directConstructionPrice = breakdownCdService.findByBcdId(bcsBcdId);
-            longDirectConstructionPrice = directConstructionPrice.getBcdPrice();;
-        } catch (NullPointerException e) {
-            // Nullの場合はゼロを代入して、以下の計算でエラーが出ない様にする
+        BreakdownCd directConstructionPrice = breakdownCdService.findByBcdId(bcsBcdId);
+        // 対象データの有無確認
+        if (directConstructionPrice != null) {
+            // 対象データがある場合
+            // ローカルフィールドに格納
+            longDirectConstructionPrice = directConstructionPrice.getBcdPrice();
+        } else {
+            // 対象データがない場合
+            //Nullの場合はゼロを代入して、以下の計算でエラーが出ない様にする
             longDirectConstructionPrice = 0L;
         }
         // Modelに格納
-        model.addAttribute("longTargePrice", longDirectConstructionPrice);
+        model.addAttribute("longTargetPrice", longDirectConstructionPrice);
 
         /** 「内訳種目の直接工事費－内訳科目の合計金額」の検算結果を取得 */
-        // 金額が入力されていない場合NullPointerExceptionを吐くのでtry-catchで対応
-        try {
-            BreakdownCs sumDirectConstructionPrice = service.findSumById(bcsBcdId);
+        // 対象データを取得
+        BreakdownCs sumDirectConstructionPrice = service.findSumById(bcsBcdId);
+        // 対象データの有無確認
+        if (sumDirectConstructionPrice != null) {
+            // 対象データがある場合
+            // ローカルフィールドに格納
             longSumDirectConstructionPrice = sumDirectConstructionPrice.getSumBcsPrice();
-        } catch (NullPointerException e) {
-            // Nullの場合はゼロを代入して、以下の計算でエラーが出ない様にする
+        } else {
+            // 対象データがない場合
+            //Nullの場合はゼロを代入して、以下の計算でエラーが出ない様にする
             longSumDirectConstructionPrice = 0L;
         }
         // 上記合計金額より直接工事費を減算して差額を算出
@@ -140,12 +108,33 @@ public class BreakdownCsController {
         model.addAttribute("defDirectConstructionPrice", defDirectConstructionPrice);
 
         /** 特定画面へ遷移 */
-        // 特定画面へ引き継ぐデータをModelに格納
-        model.addAttribute("bcsBcdId", bcsBcdId);
-        // Modelに格納
-        model.addAttribute("breakdownCs", service.findAllById(bcsBcdId));
-        // 一覧画面へ遷移（アドレス指定）
-        return "breakdown-cs/specify";
+        // GETメソッドでid入力可能のため、URLでidを直入力された場合の、対象データの有無チェックを行う
+        // 対象データを取得
+        BreakdownCd targetBreakdownCd = breakdownCdService.findByBcdId(bcsBcdId);
+        if (targetBreakdownCd != null) {
+            // 対象データがある場合
+            // 工事契約と内訳頭紙区分と内訳種目区分を取得（これらを呼びたすために内訳頭紙を最初に取得）
+            BreakdownCo breakdownCo = breakdownCoService.findByBcoId(targetBreakdownCd.getBcdBcoId());
+            ConstructionContract constructionContract = constructionContractService.findById(breakdownCo.getBcoCcId());
+            CategoryOutline categoryOutline = categoryOutlineService.findById(breakdownCo.getBcoCoId());
+            CategoryDetail categoryDetail = categoryDetailService.findById(targetBreakdownCd.getBcdCdId());
+            // Modelに格納
+            model.addAttribute("projectName", constructionContract.getProjectName());
+            model.addAttribute("coTypeName", categoryOutline.getCoTypeName());
+            model.addAttribute("cdTypeName", categoryDetail.getCdTypeName());
+            model.addAttribute("bcdTypeName", targetBreakdownCd.getBcdTypeName());
+            model.addAttribute("breakdownCs", service.findAllById(bcsBcdId));
+            model.addAttribute("bcsBcdId", bcsBcdId);
+            model.addAttribute("bcdBcoId", targetBreakdownCd.getBcdBcoId());
+            // 一覧画面へ遷移（アドレス指定）
+            return "breakdown-cs/specify";
+        } else {
+            // 対象データがない場合
+            //　エラーのフラッシュメッセージをRedirectAttributesに格納
+            redirectAttributes.addFlashAttribute("errorMessage", "対象データがありません");
+            // リダイレクト（アドレス指定）
+            return "redirect:/construction-contract/list";
+        }
 
     }
 
